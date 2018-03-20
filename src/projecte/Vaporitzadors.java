dad5165270247235ee227a88abc98845f5bf1f3b
marcmestre <marcmestre@marcmestrePC>
2018-03-20 09:46:33 +0100

    Primer commit projecte

---
+++ src/projecte/Vaporitzadors.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projecte;
+
+/**
+ *
+ * @author marcmestre
+ */
+public class Vaporitzadors {
+    private String model;
+    private String marca;
+    private int numbat;
+    private int watts;
+    private boolean electronic, omplit;
+
+    public boolean isOmplit() {
+        return omplit;
+    }
+
+    public void setOmplit(boolean omplit) {
+        this.omplit = omplit;
+    }
+    private double preu;
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public int getNumbat() {
+        return numbat;
+    }
+
+    public void setNumB(int numB) {
+        this.numbat = numbat;
+    }
+
+    public int getWatts() {
+        return watts;
+    }
+
+    public void setWatts(int watts) {
+        this.watts = watts;
+    }
+
+    public boolean isElectronic() {
+        return electronic;
+    }
+
+    public void setElectronic(boolean electronic) {
+        this.electronic = electronic;
+    }
+
+    public double getPreu() {
+        return preu;
+    }
+
+    public void setPreu(double preu) {
+        this.preu = preu;
+    }
+    
+    
+}
